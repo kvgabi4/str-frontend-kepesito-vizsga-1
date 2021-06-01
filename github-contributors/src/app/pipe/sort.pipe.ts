287b8b8f371ece7b4e06429ec6ce43f3f1214f45
kvgabi4 <kvgabi4@gmail.com>
2021-06-01 22:33:57 +0200

    sort

--- github-contributors/src/app/pipe/sort.pipe.ts
+++ github-contributors/src/app/pipe/sort.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any[] | null, key: string): any[] | null {
 
+    if (!Array.isArray(value) || !key) {
+      return value;
+    }
+
+    return value.sort((a: any, b: any): any => {
+      if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+        return b[key] - a[key];
+      } else {
+        const A: string = a[key].toString().toLowerCase();
+        const B: string = b[key].toString().toLowerCase();
+        return B.localeCompare(A);
+      }
+    })
+  }
 }
